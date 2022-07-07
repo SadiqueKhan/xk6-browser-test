15f5c850aedeef7471db24e9cc2cd3a88736602d
İnanç Gümüş <inanc.gumus@grafana.com>
2022-07-07 09:42:36 +0300

    Improve browser newPageInContext error

--- common/browser.go
+++ common/browser.go
@@ -382,7 +382,7 @@ func (b *Browser) newPageInContext(id cdp.BrowserContextID) (*Page, error) {
 	action := target.CreateTarget("about:blank").WithBrowserContextID(id)
 	tid, err := action.Do(cdp.WithExecutor(ctx, b.conn))
 	if err != nil {
-		return nil, fmt.Errorf("%T: %w", action, err)
+		return nil, fmt.Errorf("creating a blank page in browser context %q: %w", id, err)
 	}
 	// let the event handler know about the new page.
 	targetID <- tid
